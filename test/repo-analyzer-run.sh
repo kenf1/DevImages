1a59c38736ca7644592b7c9ed592d061bc65a203
kenf1 <103538771+kenf1@users.noreply.github.com>
2025-11-28 13:28:48 -0800

    Tidy repo

--- test/repo-analyzer-run.sh
+++ test/repo-analyzer-run.sh
@@ -1,7 +1,6 @@
 #expected run from repo root
 
 #!/bin/bash
-
 repo-analyzer --remote-url https://github.com/gokh4nozturk/repo-analyzer --output-format json
 mkdir -p ./data
 mv report.json ./data/report.json
